8b4ed5964c434e94439946b49f6626261725308d
JuniorD-Isael <isaeldjunior@gmail.com>
2024-09-30 21:40:07 -0300

    utilizando a biblioteca opencv para melhorar a leitura da imagem

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,13 @@ FROM python:3.12-slim
 
 WORKDIR /app
 
-RUN apt-get update && apt-get install -y tesseract-ocr
+RUN apt-get update && apt-get install -y tesseract-ocr libgl1
 
 RUN pip install poetry
 
 COPY pyproject.toml poetry.lock* /app/
-RUN poetry install --no-root --no-dev
+RUN poetry config virtualenvs.create false \
+    && poetry install --no-root --no-dev
 
 COPY . .
 
--- app/main.py
+++ app/main.py
@@ -1,19 +1,29 @@
-import io
+# import io
 from fastapi import FastAPI, UploadFile, File
 import pytesseract
-from PIL import Image
+import cv2
+import numpy as np
+# from PIL import Image
 
 app = FastAPI()
 
 @app.post('/process-image')
 async def process_image(file: UploadFile = File(...)):
     contents = await file.read()
-    image = Image.open(io.BytesIO(contents))
 
-    gray_image = image.convert('L')
+    nparr = np.frombuffer(contents, np.uint8)
+
+    # image = Image.open(io.BytesIO(contents))
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
 
-    brand_name = pytesseract.image_to_string(gray_image)
+    # gray_image = image.convert('L')
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    _, thresh_image = cv2.threshold(gray_image, 150, 255, cv2.THRESH_BINARY)
+
+    # brand_name = pytesseract.image_to_string(gray_image)
+    brand_name = pytesseract.image_to_string(thresh_image)
 
     print(f"Texto detectado: {brand_name.strip()}")
 
